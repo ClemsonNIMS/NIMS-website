c80822ee18108030411ca990531568e8edf7eb17
Collin <collin@Collins-MacBook-Pro.local>
2017-07-12 23:53:16 -0400

    Added ability to subscribe to NIMS newsletter
    Users can now subscribe to our send grid newsletter from the events
    page.

---
+++ assets/js/subscribe.js
@@ -0,0 +1,29 @@
+$("#subscribeButton").click(function(){
+  console.log("User subscribing\n");
+  var firstName = $.trim($("#inputFirstName").val());
+  var lastName = $.trim($("#inputLastName").val());
+  var email = $.trim($("#inputEmail").val());
+ 
+  if(firstName.length<=0) console.log("First name invalid\n");
+  if(lastName.length<=0) console.log("Last name invalid\n");
+  if(email.length<=0) console.log("Email invalid\n");
+
+  if(firstName.length>0 && lastName.length>0 && email.length>0){
+    var userData = '[ { "email":"' + email + '","first_name":"' + firstName + '","last_name":"' + lastName + '"} ]';
+  
+    $.ajax({
+      url: "https://api.sendgrid.com/v3/contactdb/recipients",
+      type: "post",
+      headers: {
+          "Authorization": "Bearer <<replace with token>>"
+      },
+      dataType: 'application/json',
+   	  data: userData,
+   	  success: function(data){
+   	  	//TODO add user to main list as well
+    	console.log("Added\n");
+        //console.log(data);
+    }
+    });
+  }
+});
