cb72823d1d865690c3df992118f26b279cf859ef
Collin <aevitus23@gmail.com>
2017-08-03 16:53:18 -0400

    Search params change
    Now searches all text in card, not just selections

--- public/assets/js/toggles.js
+++ public/assets/js/toggles.js
@@ -13,13 +13,9 @@ $('#resourceSearch').keypress(function(key){
 function filterResources(filter) {
 
   $('.card').each(function(ndx, card){
-    var cardResources = $.trim($(card).find(".resources").text().toLowerCase().replace(/\s+/g, ''));
-    var cardTitle = $.trim($(card).find('.card-header').text().toLowerCase().replace(/\s+/g, ''));
-    var restrictions = $.trim($(card).find('#restrictions').text().toLowerCase().replace(/\s+/g, ''));
-    var contact = $.trim($(card).find('#contact').text().toLowerCase().replace(/\s+/g, ''));
-    
-    if(cardResources.indexOf(filter)!==-1 || cardTitle.indexOf(filter)!==-1 
-        || restrictions.indexOf(filter)!==-1 || contact.indexOf(filter)!==-1){
+    var cardContent = $.trim($(card).text().toLowerCase().replace(/\s+/g, ''));
+
+      if(cardContent.indexOf(filter) !== -1){
         //Card resource list contains the filter, so show it
         this.style.display = "";
         $(this).parent().addClass("col-sm-12").addClass("col-lg-6");
@@ -29,6 +25,5 @@ function filterResources(filter) {
         //removing column styles is necessary to prevent floating cards when only one result matches
         $(this).parent().removeClass("col-sm-12").removeClass("col-lg-6");
     }
-    console.log(filter);
   });
 }
