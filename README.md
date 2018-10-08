22c2c907e781d65ee2d3dfd31e876c9f9bee44f1
Collin <aevitus23@gmail.com>
2018-10-08 16:25:30 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-https://nims-b755e.firebaseapp.com/ <br />
-Clemson Network for Innovation and Marketable Skills <br />
-Connecting students with resources
+https://CECASspark.com <br />
+Clemson University Innovation Fellows <br />
+A pitch competition for CECAS students at Clemson
--- public/assets/js/template-builder.js
+++ public/assets/js/template-builder.js
@@ -1,35 +1,3 @@
-function buildPreviewCardsOrgs(){
-	$.getJSON("/assets/orgData.json", function(orgs){
-      $.get("templates.html", function(templates){
-        var template = $(templates).filter("#card-preview-orgs").html(); 
-        var $content = $("<div class='row no-gutters'></div>");
-        for(ndx=0; ndx<orgs.length; ndx++){
-          var $card = $(Mustache.render(template, orgs[ndx]));
-          $content.append($card);
-        }
-        $("#org-previews").append($content);
-      });
-    });
-}
-
-function buildPreviewCardsResources(callback){
-  $.getJSON("/assets/orgData.json", function(orgs){
-      $.get("templates.html", function(templates){
-        var template = $(templates).filter("#card-preview-resources").html(); 
-        var $content = $("<div class='row no-gutters'></div>");
-        for(ndx=0; ndx<orgs.length; ndx++){
-          //Resource page should only include orgs with resources
-          if(!orgs[ndx].resources || !orgs[ndx].resources.length) continue;
-
-          var $card = $(Mustache.render(template, orgs[ndx]));
-          $content.append($card);
-        }
-        $("#org-previews").append($content);
-        highlightToday();
-      });
-    });
-  
-}
 
 function buildNavbar(){
     $.get("templates.html", function(templates){
@@ -43,20 +11,4 @@ function buildFooter(){
         var $template = $(templates).filter("#footer-template").html();
         $("#footer").append($template);
     });
-}
-
-//Highlights the hours for today in each resource's hours section
-var highlightToday = function (){
-  var weekday = new Array(7);
-    weekday[0] = "sunday";
-    weekday[1] = "monday";
-    weekday[2] = "tuesday";
-    weekday[3] = "wednesday";
-    weekday[4] = "thursday";
-    weekday[5] = "friday";
-    weekday[6] = "saturday";
-
-  //finds divs where the id=today, case insensitive
-  var today = weekday[new Date().getDay()];
-  $("tr[id='" + today + "' i]").addClass("today");
 }
